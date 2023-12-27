305c3da36c1e1b1a6721e8137ef44e51aa67455d
Trubiso <trubiso@users.noreply.github.com>
2023-12-27 14:27:46 +0100

    check case systems in code

--- src/checker.rs
+++ src/checker.rs
@@ -1,10 +1,13 @@
-use self::context::{check_stmt, Context};
+use self::{
+	case::{check_case_ident, Case},
+	context::{check_stmt, Context},
+};
 use crate::{common::stmt::Stmt, parser::types::ParserScope};
 use codespan_reporting::diagnostic::Diagnostic;
 use lazy_static::lazy_static;
 use std::sync::Mutex;
 
-// pub mod case;
+pub mod case;
 mod context;
 
 lazy_static! {
@@ -15,18 +18,41 @@ pub fn add_diagnostic(diagnostic: Diagnostic<usize>) {
 	DIAGNOSTICS.lock().unwrap().push(diagnostic);
 }
 
+// Case systems:
+// variables/functions -> snake_case
+// types/generics -> PascalCase
+// pure consts -> UPPER_SNAKE_CASE
+// never use camelCase
+
 fn check_inner(scope: &ParserScope, context: Context) {
 	for stmt in &scope.stmts {
 		check_stmt(stmt, &context);
 		match &stmt.value {
-			Stmt::Func { body, .. } => {
-				body.as_ref().map(|body| check_inner(&body.value, Context::Func));
+			Stmt::Create { ty_id, .. } => check_case_ident(&ty_id.value.ident, Case::SnakeCase),
+			Stmt::Set { .. } => {}
+			Stmt::Func {
+				id,
+				signature,
+				body,
+			} => {
+				check_case_ident(id, Case::SnakeCase);
+				for generic in &signature.generics.value {
+					check_case_ident(generic, Case::PascalCase);
+				}
+				for arg in &signature.args.value {
+					check_case_ident(&arg.value.ident, Case::SnakeCase);
+				}
+				match body {
+					Some(body) => check_inner(&body.value, Context::Func),
+					None => {}
+				}
 			}
-			_ => {}
+			Stmt::Return { .. } => {}
 		}
 	}
 }
 
+// TODO: check no yields in funcs
 pub fn check(scope: &ParserScope) -> Vec<Diagnostic<usize>> {
 	check_inner(scope, Context::TopLevel);
 	if DIAGNOSTICS.lock().unwrap().is_empty() {
---
+++ src/checker/case.rs
@@ -0,0 +1,101 @@
+use super::add_diagnostic;
+use crate::{common::ident::Ident, common::span::{Span, Spanned}};
+use codespan_reporting::diagnostic::{Diagnostic, Label};
+use derive_more::Display;
+
+#[derive(Display, PartialEq)]
+pub enum Case {
+	#[display(fmt = "pascal case")]
+	PascalCase,
+	#[display(fmt = "snake case")]
+	SnakeCase,
+	#[display(fmt = "snake or camel case")]
+	SnakeCamel,
+	#[display(fmt = "camel case")]
+	CamelCase,
+	#[display(fmt = "upper snake case")]
+	UpperSnakeCase,
+}
+
+fn is_uppercase(str: &str) -> bool {
+	for char in str.chars() {
+		if char.is_alphabetic() && char.is_lowercase() {
+			return false;
+		}
+	}
+	true
+}
+
+fn has_uppercase(str: &str) -> bool {
+	for char in str.chars() {
+		if char.is_alphabetic() && char.is_uppercase() {
+			return true;
+		}
+	}
+	false
+}
+
+fn begins_with_uppercase(str: &str) -> bool {
+	str.chars().next().unwrap().is_uppercase()
+}
+
+pub fn check_case(span: &Span, name: String, wanted: Case) {
+	let found = match wanted {
+		Case::PascalCase => {
+			if name.contains('_') {
+				if is_uppercase(&name) {
+					Case::UpperSnakeCase
+				} else {
+					Case::SnakeCase
+				}
+			} else if !begins_with_uppercase(&name) {
+				if has_uppercase(&name) {
+					Case::CamelCase
+				} else {
+					Case::SnakeCamel
+				}
+			} else {
+				Case::PascalCase
+			}
+		}
+		Case::SnakeCase => {
+			if has_uppercase(&name) {
+				if name.contains('_') && !is_uppercase(&name) {
+					Case::SnakeCase
+				} else if name.contains('_') {
+					Case::UpperSnakeCase
+				} else if begins_with_uppercase(&name) {
+					Case::PascalCase
+				} else {
+					Case::CamelCase
+				}
+			} else {
+				Case::SnakeCase
+			}
+		}
+		Case::UpperSnakeCase => {
+			if !is_uppercase(&name) {
+				if name.contains('_') {
+					Case::SnakeCase
+				} else {
+					Case::PascalCase
+				}
+			} else {
+				Case::UpperSnakeCase
+			}
+		}
+		_ => panic!("why"),
+	};
+	if found != wanted {
+		add_diagnostic(
+			Diagnostic::warning()
+				.with_message("wrong case system used")
+				.with_labels(vec![Label::primary(span.file_id, span.range())
+					.with_message(format!("expected {wanted}, found {found}",))]),
+		)
+	}
+}
+
+pub fn check_case_ident(ident: &Spanned<Ident>, wanted: Case) {
+	check_case(&ident.span, ident.value.to_string(), wanted)
+}
